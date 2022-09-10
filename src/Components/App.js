f5190351b60d446fae2a708a3fc65cfe40f4f83d
EslamSaeed9854 <83390999+EslamSaeed9854@users.noreply.github.com>
2022-09-10 04:31:08 +0200

    .

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import Home from "./Home";
 import { Route, Routes } from "react-router-dom";
 import PythonCourse from "./PythonCourse";
+import Navbar from './Navbar';
 function App() {
   return (
     <div>
+      <Navbar/>
       <Routes>
         <Route path="/Home/Python" element={<PythonCourse/>}></Route>
         <Route path="/Home" element={<Home/>}></Route>
