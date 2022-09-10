9ba8af6e82c44a9a865a9c5fa1942168be1085f1
EslamSaeed9854 <83390999+EslamSaeed9854@users.noreply.github.com>
2022-09-10 16:30:25 +0200

    .

--- src/Components/CourseContentItem.js
+++ src/Components/CourseContentItem.js
@@ -5,17 +5,27 @@ function CourseContentItem(props) {
   mhref += props.id;
   let mid = "collapseExample";
   mid += props.id;
+  function do_it(){
+    if(document.getElementById(mid).classList.contains("collapse")){
+      document.getElementById(mid).classList.add('active');
+      document.getElementById(mid).classList.remove('collapse');
+    }
+    else{
+      document.getElementById(mid).classList.remove('active');
+      document.getElementById(mid).classList.add('collapse');
+    }
+   
+  }
   return (
     <div>
       <p class="mb-0 w-100">
         <a
           class="btn border w-100 "
-          data-bs-toggle="collapse"
           href={mhref}
           role="button"
-          aria-expanded="false"
-          aria-controls="collapseExample"
+
           style={{ height: "50px", backgroundColor: "rgb(209, 215, 220)" }}
+          onClick={do_it}
         >
           {props.name}
         </a>
--- src/Components/CourseContents.js
+++ src/Components/CourseContents.js
@@ -1,8 +1,37 @@
 import React from "react";
+import { ArrowsExpand } from "react-bootstrap-icons";
 import CourseContentItem from "./CourseContentItem";
 import data from "./data";
 
 function CourseContent() {
+  let expnd = "Expand all sections";
+  let collps = "Collapse all sections";
+  let cur_mood = expnd;
+  function expand() {
+    for (let i = "1"; i <= "10"; i++) {
+      let cur = "collapseExample" + i;
+      document.getElementById(cur).classList.add("active");
+      document.getElementById(cur).classList.remove("collapse");
+    }
+  }
+  function collapse() {
+    for (let i = "1"; i <= "10"; i++) {
+      let cur = "collapseExample" + i;
+      document.getElementById(cur).classList.add("collapse");
+      document.getElementById(cur).classList.remove("active");
+    }
+  }
+  function do_it() {
+    if (cur_mood == expnd) {
+      expand();
+      cur_mood = collps;
+    } else {
+      collapse();
+      cur_mood = expnd;
+    }
+    document.getElementById("btn").textContent = cur_mood;
+  }
+  
   return (
     <div class="mb-5">
       <th class="col-7" style={{ fontSize: "27px" }}>
@@ -15,20 +44,27 @@ function CourseContent() {
           15 sections • 146 lectures • 14h 42m total length
         </div>
         <div class="col-1"></div>
-        <div class="col-4" style={{ textIndent: "20%"}}>
-          <button class ="fw-bold bg-white" style={{border :"0px",  fontSize: "20px", color: "blueviolet"  }}
-           onClick={ ()=>{
-           
-          }}
-           >
+        <div class="col-4" style={{ textIndent: "20%" }}>
+          <button
+          id = "btn"
+            class="fw-bold bg-white"
+            style={{ border: "0px", fontSize: "20px", color: "blueviolet" }}
+            onClick={do_it}
+          >
             {" "}
-            Expand all sections
+            {cur_mood}
           </button>
         </div>
       </div>
-      { data.map((cur_course)=>{
-       return  <CourseContentItem id = {cur_course.id} name ={cur_course.CourseName} list = {cur_course.CourseInside}/>
-     })}
+      {data.map((cur_course) => {
+        return (
+          <CourseContentItem
+            id={cur_course.id}
+            name={cur_course.CourseName}
+            list={cur_course.CourseInside}
+          />
+        );
+      })}
     </div>
   );
 }
